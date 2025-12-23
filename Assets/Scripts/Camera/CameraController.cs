939c276c2d7f53b18f28f7be8b83001e93a030a4
HuTao662 <238139190+OnionRingBurger@users.noreply.github.com>
2025-12-23 18:32:43 +0900

    詳細の方に細かく書きました。1時間半土砂下痢で鬱
    Modelの適用、Cameraが壁をすり抜けないようにする際ガクってなる問題の修正、BackPの削除、Playerの向きをTPSらしく切り替えれるように追加、Prefabの作成

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -2,54 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+namespace UnityJam
 {
-    // 自分自身の持つカメラ
-    private Camera cam;
-
-
-    [SerializeField] private float fieldOfViewDegree;
-
-    private enum FOVMode
+    public class CameraController : MonoBehaviour
     {
-        Vertical,
-        Horizontal
-    };
+        // 自分自身の持つカメラ
+        private Camera cam;
 
-    [SerializeField] private FOVMode fovMode;
 
-    private void Awake()
-    {
-        cam = GetComponent<Camera>();
-    }
+        [SerializeField] private float fieldOfViewDegree;
 
-    private void FixedUpdate()
-    {
-        // カメラのが
-
-        float currentAspectRatio = cam.aspect;
-        float verticalFov = 0;
-        switch (fovMode)
+        private enum FOVMode
         {
-            case FOVMode.Vertical: // this.fieldOfViewDegreeが垂直画角
-                verticalFov = fieldOfViewDegree;
-                break;
-            case FOVMode.Horizontal: // this.fieldOfViewDegreeが水平画角
-                verticalFov = HorizontalToVerticalFov(fieldOfViewDegree, currentAspectRatio);
-                break;
+            Vertical,
+            Horizontal
+        };
+
+        [SerializeField] private FOVMode fovMode;
+
+        private void Awake()
+        {
+            cam = GetComponent<Camera>();
         }
 
-        // Cameraの縦の画角に変更
-        cam.fieldOfView = verticalFov;
+        private void FixedUpdate()
+        {
+            // カメラのが
+
+            float currentAspectRatio = cam.aspect;
+            float verticalFov = 0;
+            switch (fovMode)
+            {
+                case FOVMode.Vertical: // this.fieldOfViewDegreeが垂直画角
+                    verticalFov = fieldOfViewDegree;
+                    break;
+                case FOVMode.Horizontal: // this.fieldOfViewDegreeが水平画角
+                    verticalFov = HorizontalToVerticalFov(fieldOfViewDegree, currentAspectRatio);
+                    break;
+            }
+
+            // Cameraの縦の画角に変更
+            cam.fieldOfView = verticalFov;
+        }
+
+        private float HorizontalToVerticalFov(float horizontalFov, float aspectRatio)
+        {
+            return 2f * Mathf.Rad2Deg * Mathf.Atan(Mathf.Tan(horizontalFov * 0.5f * Mathf.Deg2Rad) / aspectRatio);
+        }
+
+        private float VerticalToHorizontalFov(float verticalFov, float aspectRatio)
+        {
+            return 2f * Mathf.Rad2Deg * Mathf.Atan(Mathf.Tan(verticalFov * 0.5f * Mathf.Deg2Rad) * aspectRatio);
+        }
     }
 
-    private float HorizontalToVerticalFov(float horizontalFov, float aspectRatio)
-    {
-        return 2f * Mathf.Rad2Deg * Mathf.Atan(Mathf.Tan(horizontalFov * 0.5f * Mathf.Deg2Rad) / aspectRatio);
-    }
-
-    private float VerticalToHorizontalFov(float verticalFov, float aspectRatio)
-    {
-        return 2f * Mathf.Rad2Deg * Mathf.Atan(Mathf.Tan(verticalFov * 0.5f * Mathf.Deg2Rad) * aspectRatio);
-    }
 }
+
